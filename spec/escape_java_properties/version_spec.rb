115d05464b28c13a1a5bd6aaea4a6ff853bb56a1
trace-devops <60356510+trace-devops@users.noreply.github.com>
2020-01-28 10:58:42 +0000

    Update and rename spec/proper_properties/version_spec.rb to spec/escape_java_properties/version_spec.rb

---
+++ spec/escape_java_properties/version_spec.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+describe EscapeJavaProperties do
+  it "should have a version" do
+    EscapeJavaProperties::VERSION.wont_be_nil
+  end
+end
--- spec/proper_properties/version_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-describe ProperProperties do
-  it "should have a version" do
-    ProperProperties::VERSION.wont_be_nil
-  end
-end
